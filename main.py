b64abb5d40b787d6d687f266fe18e2683aae801e
zhuvry <briangiraldo123@hotmail.com>
2016-05-23 22:59:13 -0500

    inicio de proyecto

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from models.piecewise_functions import Piecewise_funtion
-from models.model_sections import Section
-if __name__ == '__main__':
-    section1 = Section(3, 5, lambda x: x**2)
-    section2 = Section(5, 7, lambda x: x + 25)
-    section3 = Section(9, 11, lambda x: x**3)
-    function = Piecewise_funtion(section1)
-    function.unir(section2)
-    function.unir(section3)
-
-    for i in range(2, 14):
-        print(function.function(i))
 
